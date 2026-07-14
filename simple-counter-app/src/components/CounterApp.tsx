51d833195ca150b10f1ee58635b93ab2ef2e3448
baye <muhammadbaye@gmail.com>
2026-07-14 15:23:49 +0100

    Added the functionality of saving count in localStorge so that it persists on page refresh.

--- simple-counter-app/src/components/CounterApp.tsx
+++ simple-counter-app/src/components/CounterApp.tsx
@@ -1,14 +1,37 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CounterDislay from "@/components/CounterDisplay";
 import CounterControls from "@/components/CounterControl";
 
 export default function CounterApp() {
   const [count, setCount] = useState(0);
+
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => { setIsMounted(true);}, []);
+  useEffect(() => {
+    if (isMounted) {
+      const savedCount = localStorage.getItem('counter-value');
+      if (savedCount !== null) {
+        setCount(Number(savedCount))
+      } 
+    }
+  }, [isMounted]);
+
+  useEffect(() => {
+    if (isMounted) {
+      localStorage.setItem('counter-value', String(count))
+    }
+  }, [count, isMounted]);
+
+  if (!isMounted) {
+    return null;
+  }
+
   const increment = () => setCount((prev) => prev + 1);
   const decrement = () => setCount((prev) => prev - 1);
   const reset = () => setCount((prev) => 0);
+  
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-500">
